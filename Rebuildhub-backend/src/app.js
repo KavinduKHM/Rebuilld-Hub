bfff9f7e57e6cbfe9578595a84efce96ef0f78c7
Kavindu Kalhara <kavinduhewamadduma@gmail.com>
2026-02-26 22:13:13 +0530

    Merged aid and disaster components

--- Rebuildhub-backend/src/app.js
+++ Rebuildhub-backend/src/app.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 
-
 const disasterRoutes = require("./routes/disasterRoutes/DisasterRoutes");
-const damageReportRoutes = require("./routes/disasterRoutes/damageReportRoutes");
+const damageReportRoutes = require("./routes/disasterRoutes/DamageReportRoutes");
 const nasaRoutes = require("./routes/disasterRoutes/nasaRoutes");
+const authRoutes = require("./routes/authRoutes/authRoutes");
+const aidRoutes = require("./routes/aidRoutes/aidRoutes");
+const weatherRoutes = require("./routes/weatherRoutes/weatherRoutes");
+
+
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 
 
@@ -16,5 +26,17 @@ app.use(express.json());
 app.use("/api/disasters", disasterRoutes);
 app.use("/api/reports", damageReportRoutes);
 app.use("/api/external", nasaRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/aids", aidRoutes);
+app.use("/api/weather", weatherRoutes);
+
+// Log incoming requests (debug)
+app.use((req, res, next) => {
+  console.log('REQ', req.method, req.originalUrl);
+  next();
+});
+
+
+
 
 module.exports = app;
