d55cb18910a0a71fa429b0e84d2fa2419a8ec2f0
Shaini Jayakody <shainijayakody8@gmail.com>
2026-02-26 18:17:08 +0530

    Update app.js

--- Rebuildhub-backend/src/app.js
+++ Rebuildhub-backend/src/app.js
@@ -7,12 +7,16 @@ app.use(express.json());
 const inventoryRoutes = require("./routes/resourceRoutes/inventoryRoute");
 const donationRoutes = require("./routes/resourceRoutes/donationRoute");
 
-app.use("/api/inventory", inventoryRoutes);
-app.use("/api/donations", donationRoutes);
+app.use("/Rebuiledhub/inventory", inventoryRoutes);
+app.use("/Rebuiledhub/donations", donationRoutes);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
   res.status(err.status || 500).json({ message: err.message || "Server Error" });
+
+  // Payment route
+const paymentRoutes = require("./routes/paymentRoutes/paymentRoute");
+app.use("/api/payment", paymentRoutes);
 });
 
 module.exports = app;
