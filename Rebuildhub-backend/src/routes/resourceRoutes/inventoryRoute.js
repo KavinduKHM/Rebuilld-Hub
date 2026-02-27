88e410c78aa250eea0be900dbfc695645fc235e6
isuriranasinghe20 <isuriranasinghe70@gmail.com>
2026-02-27 21:41:11 +0530

    Backend 80% done

--- Rebuildhub-backend/src/routes/resourceRoutes/inventoryRoute.js
+++ Rebuildhub-backend/src/routes/resourceRoutes/inventoryRoute.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../../controllers/resourceController/inventoryController");
+const authMiddleware = require("../../middlewares/authMiddleware");
 const { adminOnly } = require("../../middlewares/authMiddleware");
 
-router.post("/", adminOnly, controller.createInventory);
+router.post("/", authMiddleware, adminOnly, controller.createInventory);
 router.get("/", controller.getAllInventory);
 router.get("/:id", controller.getInventoryById);
-router.put("/:id", adminOnly, controller.updateInventory);
-router.delete("/:id", adminOnly, controller.deleteInventory);
+router.put("/:id", authMiddleware, adminOnly, controller.updateInventory);
+router.delete("/:id", authMiddleware, adminOnly, controller.deleteInventory);
 
 module.exports = router;
